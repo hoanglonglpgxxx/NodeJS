8fab3683ccb5483371205ce16e27d2acb5d175ae
Mits <hoanglonglpgxxx@gmail.com>
2024-02-06 18:02:04 +0700

    up

--- BuiltIn_Modules/httpModule.js
+++ BuiltIn_Modules/httpModule.js
@@ -1,8 +1,20 @@
 const http = require('node:http');
+const fs = require('node:fs');
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200);
-    res.end('test');
+
+    // const detailItem = {
+    //     firstName: 'A',
+    //     lastName: 'B'
+    // };
+
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    fs.createReadStream('./index.html').pipe(res);
+    const html = fs.readFileSync('./index.html', 'utf-8');
+
+    // res.end(JSON.string(detailItem));
+    // res.end('<h1>test</h1>');
+    res.end(html);
 });
 
 server.listen(3000, () => {
