04f461a1899ded70083016c0c4a55b3ef073a576
Mits <hoanglonglpgxxx@gmail.com>
2024-04-11 17:34:38 +0700

    update Error handling

--- Express/app.js
+++ Express/app.js
@@ -12,7 +12,6 @@ if (process.env.NODE_ENV === 'development') {
 app.use(express.json());//express.json là 1 middleware
 app.use(express.static(`${__dirname}/public`));
 
-
 app.use((req, res, next) => {
     req.requestTime = new Date().toISOString();
     next();
@@ -32,4 +31,31 @@ app.delete('/api/v1/tours/:id', deleteTour); */
 
 app.use('/api/v1/tours', tourRouter); //method : Mounting Router
 app.use('/api/v1/user', userRouter);
+
+//run for all HTTP METHODS
+app.all('*', (req, res, next) => { //handling unhandled routes
+    /* res.status(404).json({
+        status: 'fail',
+        message: `Can't find ${req.originalUrl} on this server`
+    }); */
+
+    const err = new Error(`Can't find ${req.originalUrl} on this server`);
+    err.status = 'fail';
+    err.statusCode = 404;
+
+    next(err);
+});
+
+//GLOBAL ERROR HANDLING MIDDLEWARE
+//tự nhảy vào middleware này khi lỗi
+app.use((err, req, res, next) => {
+    err.statusCode = err.statusCode || 500;
+    err.status = err.status || 'error';
+
+    res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message
+    });
+});
+
 module.exports = app;
