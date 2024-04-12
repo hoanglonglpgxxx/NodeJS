177e0f619c3b96e178043df47ebcd25477add66b
Mits <hoanglonglpgxxx@gmail.com>
2024-04-12 16:57:24 +0700

    update global error handler

--- Express/app.js
+++ Express/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 
 const app = express();
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -34,28 +36,11 @@ app.use('/api/v1/user', userRouter);
 
 //run for all HTTP METHODS
 app.all('*', (req, res, next) => { //handling unhandled routes
-    /* res.status(404).json({
-        status: 'fail',
-        message: `Can't find ${req.originalUrl} on this server`
-    }); */
-
-    const err = new Error(`Can't find ${req.originalUrl} on this server`);
-    err.status = 'fail';
-    err.statusCode = 404;
-
-    next(err);
+    next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
 });
 
 //GLOBAL ERROR HANDLING MIDDLEWARE
 //tự nhảy vào middleware này khi lỗi
-app.use((err, req, res, next) => {
-    err.statusCode = err.statusCode || 500;
-    err.status = err.status || 'error';
-
-    res.status(err.statusCode).json({
-        status: err.status,
-        message: err.message
-    });
-});
+app.use(globalErrorHandler);
 
 module.exports = app;
