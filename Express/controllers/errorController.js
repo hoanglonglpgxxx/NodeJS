4f8bba30cabe4552854da2bf6603065eb735fcf4
Mits <hoanglonglpgxxx@gmail.com>
2024-04-22 17:41:03 +0700

    handle invalid db IDs and err handling on dev/prod environment handle invalid db IDs đang lỗi

--- Express/controllers/errorController.js
+++ Express/controllers/errorController.js
@@ -1,9 +1,48 @@
+const AppError = require('../utils/appError');
+
+const handleCastErrorDB = err => {
+    const message = `Invalid ${err.path}: ${err.value}`;
+    return new AppError(message, 400);
+};
+
+const sendErrorDev = (err, res) => {
+    res.status(err.statusCode).json({
+        status: err.status,
+        error: err,
+        message: err.message,
+        stack: err.stack
+    });
+};
+
+const sendErrorProd = (err, res) => {
+    //Operation/trusted err: send msg to client
+    if (err.isOperational) {
+        res.status(err.statusCode).json({
+            status: err.status,
+            message: err.message,
+        });
+        //Programming or other unknown err: dont leak err details
+    } else {
+        console.error('ERROR ', err);
+
+        res.status(500).json({
+            status: 'error',
+            message: 'Something wrong!!!'
+        });
+    }
+};
+
 module.exports = (err, req, res, next) => {
     err.statusCode = err.statusCode || 500;
     err.status = err.status || 'error';
 
-    res.status(err.statusCode).json({
-        status: err.status,
-        message: err.message
-    });
+    if (process.env.NODE_ENV === 'development') {
+        sendErrorDev(err, res);
+    } else if (process.env.NODE_ENV === 'production') {
+        let errorDB = { ...err };
+
+        if (errorDB.name === 'CastError') errorDB = handleCastErrorDB(errorDB);
+
+        sendErrorProd(errorDB, res);
+    }
 };
