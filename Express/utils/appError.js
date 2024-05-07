af3fe773a41ff6573a73d82d9479b32e4dab2290
Mits <hoanglonglpgxxx@gmail.com>
2024-05-07 17:42:19 +0700

    update documentation AppError class

--- Express/utils/appError.js
+++ Express/utils/appError.js
@@ -1,3 +1,10 @@
+/**
+ * @class AppError
+ * @description Create a new class AppError that extends the Error class, replace Error class for all folders
+ * @param {string} message - The error message.
+ * @param {number} statusCode - The error status code.
+ * @returns {object} - An instance of the AppError class.
+ */
 class AppError extends Error {
     constructor(message, statusCode) {
         super(message);
