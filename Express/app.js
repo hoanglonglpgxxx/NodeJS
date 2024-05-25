68c26b857601c91c5a5ffd6c329efd5ca76e4a07
Mits <hoanglonglpgxxx@gmail.com>
2024-05-25 11:58:54 +0700

    remove unused files, update base.pug

--- Express/app.js
+++ Express/app.js
@@ -70,7 +70,10 @@ app.patch('/api/v1/tours/:id', updateTour);
 
 app.delete('/api/v1/tours/:id', deleteTour); */
 app.get('/', (req, res) => {
-    res.status(200).render('base');
+    res.status(200).render('base', {
+        tour: 'The Forest Hiker',
+        user: 'Jonas'
+    });
 });
 
 app.use('/api/v1/tours', tourRouter); //method : Mounting Router
