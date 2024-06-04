a52f70f221774445463205839cf964a50895c3da
Mits <hoanglonglpgxxx@gmail.com>
2024-06-04 13:40:39 +0700

    feat: Add nested routes for reviews in tourRoutes.js

--- Express/routes/tourRoutes.js
+++ Express/routes/tourRoutes.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const tourController = require('../controllers/tourController');
 const authController = require('../controllers/authController');
+const reviewRouter = require('./reviewRoutes');
 
 const router = express.Router(); // là 1 middleware
 
 // router.param('id', tourController.checkID);
+
+router.use('/:tourId/reviews', reviewRouter);
 
 router.route('/top-5-cheap').get(tourController.aliasTopTours, tourController.getAllTours);
 
