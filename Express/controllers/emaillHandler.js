d0f9c153016f40879230179064c98d36fbbec5e7
Mits <hoanglonglpgxxx@gmail.com>
2024-07-01 17:30:10 +0700

    building complex email handler, create new Email class to send email

--- Express/controllers/emaillHandler.js
+++ Express/controllers/emaillHandler.js
@@ -1,25 +1,64 @@
 const nodemailer = require('nodemailer');
+const pug = require('pug');
+const htmlToText = require('html-to-text');
+// new Email(user, url).sendWelcome();
 
-const sendEmail = async options => {
-    //1. create a transporter
-    const transporter = nodemailer.createTransport({
-        host: process.env.EMAIL_HOST,
-        port: process.env.EMAIL_PORT,
-        auth: {
-            user: process.env.EMAIL_USERNAME,
-            pass: process.env.EMAIL_PASSWORD
+/**
+ * @class Email
+ * @description Send email to user
+ * @param {Object} user - user object
+ * @param {String} url - url to send
+ * @returns {Object} - email object
+ * @example new Email(user, url).sendWelcome();
+ */
+module.exports = class Email {
+    constructor(user, url) {
+        this.to = user.email;
+        this.firstName = user.name.split(' ')[0];
+        this.url = url;
+        this.from = `MitsNgeiTher <${process.env.EMAIL_FROM}>`;
+    }
+
+
+    newTransport() {
+        if (process.env.NODE_ENV === 'production') {
+            //sendgrid
+            return 1;
         }
-    });
-    //2. define email options
-    const mailOptions = {
-        from: 'MitsNgeiTher <hoanglonglpgxxx@gmail.com>',
-        to: options.email,
-        subject: options.subject,
-        text: options.message,
-        //html: options.html
-    };
-    //3. actually send email
-    await transporter.sendMail(mailOptions);
-};
 
-module.exports = sendEmail;
+        return nodemailer.createTransport({
+            host: process.env.EMAIL_HOST,
+            port: process.env.EMAIL_PORT,
+            auth: {
+                user: process.env.EMAIL_USERNAME,
+                pass: process.env.EMAIL_PASSWORD
+            }
+        });
+    }
+
+    //Send the actual email
+    async send(template, subject) {
+        //1. Render HTML based on pug template
+        const html = pug.renderFile(`${__dirname}/../views/email/${template}.pug}`, {
+            firstName: this.firstName,
+            url: this.url,
+            subject
+        });
+
+        //2. Define email options
+        const mailOptions = {
+            from: this.form,
+            to: this.to,
+            subject,
+            html,
+            text: htmlToText.fromString(html),
+        };
+
+        //3. Create a transport and send email
+        await this.newTransport().sendMail(mailOptions);
+    }
+
+    async sendWelcome() {
+        await this.send('welcome', 'Welcome to the MitsNgeiTher Family!');
+    }
+};
