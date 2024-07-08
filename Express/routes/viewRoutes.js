b8a9c4414878833d5d6d157f36164b92bf4f7a5b
Mits <hoanglonglpgxxx@gmail.com>
2024-07-08 17:35:11 +0700

    create booking with bookingModel, now always try to create booking document when go to tour's url

--- Express/routes/viewRoutes.js
+++ Express/routes/viewRoutes.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const { getOverview, getTour, getLoginForm, getAccount, updateUserData } = require('../controllers/viewsController');
 const authController = require('../controllers/authController');
+const bookingController = require('../controllers/bookingController');
 
 const router = express.Router();
 
-router.get('/', authController.isLoggedIn, getOverview);
+router.get('/',
+    bookingController.createBookingCheckout,
+    authController.isLoggedIn,
+    getOverview
+);
 router.get('/tour/:slug', authController.isLoggedIn, getTour);
 router.get('/login', authController.isLoggedIn, getLoginForm);
 router.get('/me', authController.protect, getAccount);
