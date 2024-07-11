a8571eb96e85639962d5ad6207c4f42d4067bfe8
Mits <hoanglonglpgxxx@gmail.com>
2024-07-11 16:19:23 +0700

    fix err

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -72,6 +72,8 @@ exports.webhookCheckout = (req, res, next) => {
 
     if (event.type === 'checkout.session.completed') {
         createBookingCheckout(event.data.object);
+
+        res.status(200).json({ received: true });
     }
 };
 
